882aba0d7131ccf286d68cb137691340b40e5ad6
anaclaudiabarreto <80101582+anaclaudiabarreto@users.noreply.github.com>
2022-03-07 22:17:30 -0300

    Enviando os desafios de código para o github

---
+++ DesafioQuadradoCubo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DesafioQuadradoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int a = inicio;
+                int b = a * a;
+                int c = a * b;
+                Console.Write(a+" ");
+                Console.Write(b+" ");
+                Console.WriteLine(c+" ");
+                inicio++;
+
+
+            }
+        }
+    }
+}
---
+++ DesafioSomaPares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesafioSomaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    x = (x + 4) * 5;
+                    Console.WriteLine(x);
+                    x = int.Parse(Console.ReadLine());
+                }
+
+                else
+                {
+                    x = x + 1;
+                    x = (x + 4) * 5;
+                    Console.WriteLine(x);
+                    x = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
